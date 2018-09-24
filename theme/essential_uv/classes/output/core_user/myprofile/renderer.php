7721e38c2e93af9793db0b6e54031d9c0bbbabf1
Iader E. Garcia G <iadergg@gmail.com>
2018-09-24 16:36:15 -0500

    Testing

--- theme/essential_uv/classes/output/core_user/myprofile/renderer.php
+++ theme/essential_uv/classes/output/core_user/myprofile/renderer.php
@@ -13,15 +13,11 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 namespace theme_essential_uv\output\core_user\myprofile;
 use \core_user\output\myprofile\node;
 use moodle_url;
-
 defined('MOODLE_INTERNAL') || die();
-
 class renderer extends \core_user\output\myprofile\renderer {
-
     /**
      * Render a node.
      *
@@ -30,14 +26,10 @@ class renderer extends \core_user\output\myprofile\renderer {
      * @return string
      */
     public function render_node(node $node) {
-
         global $CFG;
-
         $return = '';
-
         $userid = OPTIONAL_PARAM('id', 0, PARAM_INT);
         $allcourses = OPTIONAL_PARAM('showallcourses', 0, PARAM_INT);
-
         // Al entrar al nodo 'Perfiles de curso' se consultan los cursos en los cuales está 
         // matriculado el usuario, para su posterior ordenamiento
         if($node->title == "Perfiles de curso"){
@@ -46,7 +38,6 @@ class renderer extends \core_user\output\myprofile\renderer {
             $teacher_training_courses_array = array();
             $no_regular_courses_array = array();
             $html_to_return = "";
-
             // Contadores y valores máximos para controlar la cantidad de cursos a visualizar
             $counter_classroom_courses = 0;
             $counter_teacher_training_courses = 0;
@@ -54,10 +45,8 @@ class renderer extends \core_user\output\myprofile\renderer {
             $max_classroom_courses = 5;
             $max_teacher_training_courses = 2;
             $max_no_regular_courses = 2;
-
             // Se separan los cursos en tres arreglos, uno para los cursos presenciales,
             // cursos formación docente y cursos no regulares
-
             foreach($courses as $course){
                 if($course->category > 30000){
                     $classroom_courses_array[$course->id] = $course;
@@ -67,19 +56,15 @@ class renderer extends \core_user\output\myprofile\renderer {
                     $no_regular_courses[$course->id] = $course;
                 }
             }            
-
             // Ordenamiento de los arreglos de cursos
             krsort($classroom_courses_array);
             krsort($teacher_training_courses_array);
             krsort($no_regular_courses_array);
-
             if(count($classroom_courses_array) > 0){
                 $html_to_return = "<br>";
                 $html_to_return .= "<b>Cursos presenciales: </b><br>";
                 $html_to_return .= "<ul style='list-style-type: disc'>";
-
                 foreach($classroom_courses_array as $course){
-
                     $url = new moodle_url($CFG->wwwroot . '/course/view.php', array('id' => $course->id));
                     
                     $html_to_return .= "<li>";
@@ -87,7 +72,6 @@ class renderer extends \core_user\output\myprofile\renderer {
                     $html_to_return .= $course->shortname." ".$course->fullname;
                     $html_to_return .= "</a>";
                     $html_to_return .= "</li>";
-
                     // Validación necesaria para la opción 'Ver más'
                     if(!$allcourses){
                         if($counter_classroom_courses < $max_classroom_courses){
@@ -100,13 +84,10 @@ class renderer extends \core_user\output\myprofile\renderer {
     
                 $html_to_return .= "</ul></br>";
             }
-
             if(count($teacher_training_courses_array) > 0){
                 $html_to_return .= "<b>Formación docente: </b><br>";
                 $html_to_return .= "<ul style='list-style-type: disc'>";
-
                 foreach($teacher_training_courses_array as $course){
-
                     $url = new moodle_url($CFG->wwwroot . '/course/view.php', array('id' => $course->id));
                     
                     $html_to_return .= "<li>";
@@ -114,7 +95,6 @@ class renderer extends \core_user\output\myprofile\renderer {
                     $html_to_return .= $course->shortname." ".$course->fullname;
                     $html_to_return .= "</a>";
                     $html_to_return .= "</li>";
-
                     // Validación necesaria para la opción 'Ver más'
                     if(!$allcourses){
                         if($counter_teacher_training_courses < $max_teacher_training_courses){
@@ -124,16 +104,13 @@ class renderer extends \core_user\output\myprofile\renderer {
                         }
                     }
                 }
-
                 $html_to_return .= "</ul><br>";
             }
             
             if(count($no_regular_courses_array) > 0){
                 $html_to_return .= "<b>Cursos no regulares: </b><br>";
                 $html_to_return .= "<ul style='list-style-type: disc'>";
-
                 foreach($no_regular_courses_array as $course){
-
                     $url = new moodle_url($CFG->wwwroot . '/course/view.php', array('id' => $course->id));
                     
                     $html_to_return .= "<li>";
@@ -141,7 +118,6 @@ class renderer extends \core_user\output\myprofile\renderer {
                     $html_to_return .= $course->shortname." ".$course->fullname;
                     $html_to_return .= "</a>";
                     $html_to_return .= "</li>";
-
                     // Validación necesaria para la opción 'Ver más'
                     if(!$allcourses){
                         if($counter_no_regular_courses < $max_no_regular_courses){
@@ -151,10 +127,8 @@ class renderer extends \core_user\output\myprofile\renderer {
                         }
                     }
                 }
-
                 $html_to_return .= "</ul><br>";
             }
-
             if(!$allcourses){
                 $url_all_courses = new moodle_url($CFG->wwwroot.'/user/profile.php', array('id' => $userid, 'showallcourses' => 1));
                 $html_to_return .= "<dd><a href='".$url_all_courses."'>".get_string('viewmore')."</a></dd>";
@@ -162,7 +136,6 @@ class renderer extends \core_user\output\myprofile\renderer {
                 $url_less_courses = new moodle_url($CFG->wwwroot.'/user/profile.php', array('id' => $userid, 'showallcourses' => 0));
                 $html_to_return .= "<dd><a href='".$url_less_courses."'>".get_string('viewless')."</a></dd>";
             }
-
             $new_node = new node($node->parentcat,
                                  $node->name,
                                  $node->title,
@@ -171,9 +144,7 @@ class renderer extends \core_user\output\myprofile\renderer {
                                  $html_to_return,
                                  $node->icon,
                                  $node->classes);
-
             $node = $new_node;
-
         }
  
         if (is_object($node->url)) {
@@ -191,7 +162,6 @@ class renderer extends \core_user\output\myprofile\renderer {
             // There is some content to display below this make this a header.
             $return = \html_writer::tag('dt', $header);
             $return .= \html_writer::tag('dd', $content);
-
             $return = \html_writer::tag('dl', $return);
             if ($classes) {
                 $return = \html_writer::tag('li', $return, array('class' => 'contentnode ' . $classes));
@@ -202,8 +172,6 @@ class renderer extends \core_user\output\myprofile\renderer {
             $return = \html_writer::span($header);
             $return = \html_writer::tag('li', $return, array('class' => $classes));
         }
-
         return $return;
     }
-
 }
